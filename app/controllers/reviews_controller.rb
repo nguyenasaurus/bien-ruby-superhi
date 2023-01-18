bdbeef1564256a396071ec3dda2f87d07dd81a18
Justine Nguyen <justinexnguyen@gmail.com>
2023-01-18 15:13:46 +0000

    feat: add indivual review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,29 @@
 class ReviewsController < ApplicationController
   def index
       # list page for reviews
-      @reviews = ["Kodiak", "Bonobo", "O'Brien's"]
+      @reviews = Review.all
   end
 
   def new
-    
+    # form for a new review
+    @review = Review.new
+  end
+
+  def create
+    # get form info and add to the database
+    @review = Review.new(params.require(:review).permit(:title, :body, :score))
+
+    # save to the database
+    @review.save
+
+    # redirect
+
+    redirect_to root_path
+  end
+
+  def show
+    # individual review page
+    @review = Review.find(params[:id])
   end
 
 end
