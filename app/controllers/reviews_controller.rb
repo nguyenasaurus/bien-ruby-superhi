a6ef883f2912155ea64c72498430cecd371efcd3
Justine Nguyen <justinexnguyen@gmail.com>
2023-01-18 14:43:52 +0000

    feat: add view for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,11 @@
 class ReviewsController < ApplicationController
-    def index
-        # list page for reviews
-        
-    end
+  def index
+      # list page for reviews
+      @reviews = ["Kodiak", "Bonobo", "O'Brien's"]
+  end
+
+  def new
+    
+  end
 
 end
