1272ce45c223702e1ddad61cde065d2a801eedd7
Justine Nguyen <justinexnguyen@gmail.com>
2023-01-19 17:34:20 +0000

    feat: seo friendly urls

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :body, length: { minimum: 10 }
   validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
+  def to_param
+    id.to_s + "-" + title.parameterize
+  end
 end
