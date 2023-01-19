5e444c02a25f61c4a1db99f3d9dc0b0b5c0891fe
Justine Nguyen <justinexnguyen@gmail.com>
2023-01-19 17:44:15 +0000

    feat: add fields and validation for restaurant and cuisine

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -56,7 +56,7 @@ class ReviewsController < ApplicationController
   end
 
   def review_form_params
-    params.require(:review).permit(:title, :body, :score)
+    params.require(:review).permit(:title, :restaurant, :cuisine, :body, :score)
   end
 
 end
